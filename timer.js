31ae31de397fdab1337a9d9315532415f312f578
Jakub-Chojnacki <jakub.chojnacki27Gmail.com>
2020-10-29 23:28:57 +0100

    reset to default

--- timer.js
+++ timer.js
@@ -1,8 +1,17 @@
-let x = document.getElementById('main');
-const guzik = document.getElementById('guzik');
+// Tutaj będą odwołania do elementów HTML (DOM)
+const default_settings = document.getElementById('default'); // guzik do default settings
+const main_time = document.getElementById('main'); 
+const short_time = document.getElementById('short');
+const long_time = document.getElementById('long');
+const cycles = document.getElementById('cycles');
 
-function main() {
-  x.setAttribute('value', '8');
-}
 
-guzik.addEventListener("click", main);
+function reset_settings(){
+  main_time.value = "25";
+   short_time.value = "5";
+   long_time.value = "15";
+   cycles.value = "3";
+   console.log('clicked');
+} 
+
+default_settings.addEventListener('click', reset_settings);
